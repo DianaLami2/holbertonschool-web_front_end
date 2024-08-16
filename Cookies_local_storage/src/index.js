500f44de4a8eaa54633ade23ae4f80c7c47533c5
DianaLami2 <dianalami18@gmail.com>
2024-08-16 20:55:12 +0200

    Cookies Project

---
+++ Cookies_local_storage/src/index.js
@@ -0,0 +1,21 @@
+document.getElementById("loginForm").addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const name_val = document.getElementById("fname").value;
+  const email_val = document.getElementById("email").value;
+  setCookies(name_val, email_val);
+}
+
+function setCookies(name, email) {
+  const d = new Date();
+  d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+  const expires = d.toUTCString();
+  document.cookie = `firstname=${name}; email=${email}; expires=${expires};`;
+}
+
+
+document.getElementById("showCookie").addEventListener('click', handleShow);
+function handleShow() {
+  document.getElementById("cookieContainer").innerHTML = document.cookie;
+  console.log("handleShow is clicked:",);
+}
